fd793a2625c4908c375afecc712f9c5c072235b0
dikuw <michael@dikuw.com>
2021-07-19 15:41:16 -0400

    renamed file

--- client/cypress/integration/SRT-7_normal_wo_CO.spec.js
+++ client/cypress/integration/SRT-7_normal_wo_CO.spec.js
@@ -8,7 +8,7 @@ const tenant = 'https://test-company.qms-dry-run.nemedio.com';
 beforeEach(() => {
   cy.intercept('POST', '/graphql', (req) => {
     // Queries
-    aliasQuery(req, 'standardOperatingProceduresQuery')
+    aliasQuery(req, 'standardOperatingProceduresQuery');
     aliasQuery(req, 'ItemTypeMetadata');
 
     // Mutations
@@ -35,13 +35,16 @@ describe('SRT-7 Generic Workflow Normal Path without Change Order', () => {
     cy.get('#item-name-input').type(`${itemNamePrefix}${new Date().toLocaleTimeString([], { hour: '2-digit', minute: "2-digit", hour12: false })}`);                                                                             
     cy.get('#item-create-btn').click();
     cy.wait('@gqlcreateItemMutation');
+    //  Error without the above line: Uncaught (in promise) Error: GraphQL error: Query.documents: access denied
   })
 
   it('Opens the item', () => {
     cy.wait('@gqlstandardOperatingProceduresQueryQuery');
+    //  Error without the above line: Error: Cannot retrieve "standardOperatingProcedure": GraphQL error: Query.itemType: access denied
     cy.contains('View Item').click();
     // cy.wait('@gqlstandardOperatingProceduresQueryQuery');
     // cy.wait('@gqlItemTypeMetadataQuery');
+    //  Error with any combination of the above 2 lines: Error: Cannot retrieve "standardOperatingProcedure": GraphQL error: Query.itemType: access denied
   })
 
   it('Verifies the item is in Draft status', () => {
