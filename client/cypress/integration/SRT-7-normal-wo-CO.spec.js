ecebeef90a28d9d06fe6c172ea7e6be399f1f019
dikuw <michael@dikuw.com>
2021-07-19 15:16:17 -0400

    initial commit

---
+++ client/cypress/integration/SRT-7-normal-wo-CO.spec.js
@@ -0,0 +1,52 @@
+import { aliasQuery, aliasMutation } from '../utils/graphql-test-utils'
+
+const users = require('../fixtures/users');
+const itemTypes = require('../fixtures/itemTypes');
+const itemNamePrefix = 'SRT-7noCO';
+const tenant = 'https://test-company.qms-dry-run.nemedio.com';
+
+beforeEach(() => {
+  cy.intercept('POST', '/graphql', (req) => {
+    // Queries
+    aliasQuery(req, 'standardOperatingProceduresQuery')
+    aliasQuery(req, 'ItemTypeMetadata');
+
+    // Mutations
+    aliasMutation(req, 'createItem')
+  })
+})
+
+describe('SRT-7 Generic Workflow Normal Path without Change Order', () => {
+
+  it('Opens tenant', () => {
+    cy.visit(`${tenant}}/login`);
+  })
+
+  it('Logs in as user without the Owner or Approver role', () => {
+    cy.get('#username').type(users[0].user);
+    cy.get('#password').type('testpass0');
+    cy.get('[data-testid="login-button"]').click();
+  })
+
+  it('Creates a new item', () => {
+    cy.get('#create-item-button').click();
+    cy.get('#mui-component-select-itemType').click();
+    cy.get(`[data-value="${itemTypes[0].dataValue}"]`).click();
+    cy.get('#item-name-input').type(`${itemNamePrefix}${new Date().toLocaleTimeString([], { hour: '2-digit', minute: "2-digit", hour12: false })}`);                                                                             
+    cy.get('#item-create-btn').click();
+    cy.wait('@gqlcreateItemMutation');
+  })
+
+  it('Opens the item', () => {
+    cy.wait('@gqlstandardOperatingProceduresQueryQuery');
+    cy.contains('View Item').click();
+    // cy.wait('@gqlstandardOperatingProceduresQueryQuery');
+    // cy.wait('@gqlItemTypeMetadataQuery');
+  })
+
+  it('Verifies the item is in Draft status', () => {
+
+  })
+
+
+})
