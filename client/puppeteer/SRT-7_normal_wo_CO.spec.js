000d99a3ca120fa133aba274345eb506e3414c43
dikuw <michael@dikuw.com>
2021-07-20 11:27:27 -0400

    initial commit

---
+++ client/puppeteer/SRT-7_normal_wo_CO.spec.js
@@ -0,0 +1,5 @@
+const users = require('../data/users');
+const itemTypes = require('../data/itemTypes');
+
+const itemNamePrefix = 'SRT-7noCO';
+const tenant = 'https://test-company.qms-dry-run.nemedio.com';
