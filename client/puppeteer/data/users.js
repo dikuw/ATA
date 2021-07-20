e9abb97064982ff87a1373ae4f6ae409b8ce9d5c
dikuw <michael@dikuw.com>
2021-07-20 11:27:20 -0400

    initial commit

---
+++ client/puppeteer/data/users.js
@@ -0,0 +1,28 @@
+const users = [
+    {
+      user: "biz_lead_user",
+      userName: "Barry Business",
+    },
+    {
+      user: "qa_lead_user",
+      userName: "Quincy Adams",
+    },
+    {
+      user: "pm_user",
+      userName: "Patty Pearson",
+    },
+    {
+      user: "reg_aff_lead_user",
+      userName: "Roger Roger",
+    },
+    {
+      user: "lead_eng_user",
+      userName: "Lisa Leader",
+    },
+    {
+      user: "tech_lead_user",
+      userName: "Tammy Tech",
+    },
+  ]
+
+  export default users;
