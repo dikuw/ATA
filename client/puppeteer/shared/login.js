cccd732ee851559b40bc1f8d54b5243852b35d0c
dikuw <michael@dikuw.com>
2021-07-26 09:43:26 -0400

    removed unneeded reference

--- client/puppeteer/shared/login.js
+++ client/puppeteer/shared/login.js
@@ -1,8 +1,7 @@
-const puppeteer = require('puppeteer');
-
 const password = 'testpass0';
 
 exports.login = async (page, username) => {
+  await page.waitForSelector('#username');
   await page.type("#username", username);
   await page.type("#password", password);
   await page.click('[data-testid="login-button"]');
