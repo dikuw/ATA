b6c0fd076aecc3f0181fc88e7b3acf1c4abf8421
dikuw <michael@dikuw.com>
2021-08-02 15:12:48 -0400

    layout

--- client/src/components/ItemTypePicker.js
+++ client/src/components/ItemTypePicker.js
@@ -1,4 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  max-width: 6vw;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin: 30px auto;
+  padding: 4px;
+`;
+
+const Item = styled.div`
+
+`;
 
 export default function ItemTypePicker(props) {
   if (props.itemTypes.length < 1) {
@@ -6,11 +20,16 @@ export default function ItemTypePicker(props) {
   }
   if (props.itemTypes) {
     return (
-      <ul>
+      <Container>
         {props.itemTypes.map( (itemType) => 
-          <li key={itemType.srt1ID} >{itemType.itemPrefix}</li> 
+          <Item>
+            <label>
+              <input type="checkbox" id={itemType.srt1ID} name={itemType.title} value={itemType.itemPrefix} />
+              <span>{itemType.itemPrefix}</span>
+            </label>
+          </Item>
         )}
-      </ul>
+      </Container>
     );
   }
 }
