bfb36616f89c6edd8648162e417e40826f79ad9d
dikuw <michael@dikuw.com>
2021-08-03 10:37:12 -0400

    add try catch

--- server/controllers/testController.js
+++ server/controllers/testController.js
@@ -4,6 +4,11 @@ const { SRT71 } = require('../tests/SRT-7-1_normal_w_CO');
 const { SRT72 } = require('../tests/SRT-7-2_normal_wo_CO');
 
 exports.testRunner = async (req, res) => {
-  await SRT72(req.body.itemPrefix);
+  try {
+    await SRT72(req.body.itemPrefix);
+  } catch (err) {
+    console.log(err);
+  }
+  
   return res.json({ success: true });
 }
