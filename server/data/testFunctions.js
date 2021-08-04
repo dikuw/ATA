f2fd2d5294393281e7955983623e09ab4f790447
dikuw <michael@dikuw.com>
2021-08-04 15:29:37 -0400

    added itemTypeFilter property

--- server/data/testFunctions.js
+++ server/data/testFunctions.js
@@ -3,10 +3,12 @@ exports.testFunctions = [
     testID: 'SRT71',
     testTitle: 'SRT-7-1 Normal Path with CO',
     testFunction: 'SRT-7-1_normal_w_CO',
+    itemTypeFilter: 'Generic Non-Singletons',
   },
   {
     testID: 'SRT72',
     testTitle: 'SRT-7-2 Normal Path without CO',
     testFunction: 'SRT-7-2_normal_wo_CO',
+    itemTypeFilter: 'Generic Non-Singletons',
   },
 ]
