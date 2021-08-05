522b67be572ac28e49219e53831dc511cce8e0c7
dikuw <michael@dikuw.com>
2021-08-05 09:43:19 -0400

    clean-up

--- server/controllers/testController.js
+++ server/controllers/testController.js
@@ -1,14 +1,11 @@
-const { SRT71 } = require('../tests/SRT-7-1_normal_w_CO');
-const { SRT72 } = require('../tests/SRT-7-2_normal_wo_CO');
-const { SRT73 } = require('../tests/SRT-7-3_nullifications');
-const { SRT74 } = require('../tests/SRT-7-4_retirement_w_CO');
-
 const { Handler } = require('../tests/index');
 
 exports.testRunner = async (req, res) => {
-  let response = [];
   const items = req.body.itemPrefix;
   const [ test ] = req.body.testFunction;
+  
+  let response = [];
+  
   for (const item of items) {
     try {
       await Handler[test](item);
