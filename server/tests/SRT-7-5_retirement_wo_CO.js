8abfae1a46f83d7e355766f93de40883a4fc0af3
dikuw <michael@dikuw.com>
2021-08-05 09:50:18 -0400

    added SRT75

--- server/tests/SRT-7-5_retirement_wo_CO.js
+++ server/tests/SRT-7-5_retirement_wo_CO.js
@@ -1,26 +1,22 @@
 //  ** SRT-7 Generic Workflow **  //
 //  This script tests the Generic workflow per SRT-7
-//  4. Retirement with CO
+//  5. Retirement without CO
 //
 //  Any item type using the Generic workflow that is not a singleton can be tested
-//  Run the test with 'SRT71("xyz")' where "xyz" is the prefix of the item type
+//  Run the test with 'SRT75("xyz")' where "xyz" is the prefix of the item type
 
 const puppeteer = require('puppeteer');
 
 const { tenant } = require('../data/tenant');
 
-const changeOrderOwner = "pm_user";
-const changeOrderApprover = "qa_lead_user";
-
 const { login, logout, createItem, openTableView } = require('../shared/shared');
 const { selectTableViewLastChild } = require('../shared/shared');
-const { draftToReadyForClosure, readyForClosureToClosed } = require('../shared/shared');
 const { draftToRetirementInitiated } = require('../shared/shared');
-const { retirementInitiatedToApprovedRetirement } = require('../shared/shared');
+const { retirementInitiatedToRetired } = require('../shared/shared');
 const { createDoc } = require('../shared/createOutput');
 const { getItemType } = require('../shared/helpers');
 
-exports.SRT74 = async (prefix) => {
+exports.SRT75 = async (prefix) => {
 
   const switchUser = async (page, user, module, headerCategory, category) => {
     await logout(page);
@@ -31,7 +27,7 @@ exports.SRT74 = async (prefix) => {
 
   let itemType = getItemType(prefix)[0];
 
-  const itemNamePrefix = 'SRT-7 4. Retirement with CO';
+  const itemNamePrefix = 'SRT-7 5. Retirement without CO';
 
   const browser = await puppeteer.launch({ 
     headless: false,
@@ -45,15 +41,10 @@ exports.SRT74 = async (prefix) => {
 
   const { itemPrefix, dataValue, user, owner, approver, module, headerCategory, category, sort } = itemType;
 
-  console.log(`Testing SRT-7 4. Retirement with CO ${sort}. ${itemPrefix}...`);
+  console.log(`Testing SRT-7 5. Retirement without CO ${sort}. ${itemPrefix}...`);
 
   let results = [];
   let screenshot = "";
-
-  //  first create the change order for testing
-  await login(page, changeOrderOwner);
-  await createItem(page, "change_order", "SRT-7 4. CO");
-  await logout(page);
 
   await login(page, user);
   //  SRT-7.4 -- Does Not Exist -> Draft
@@ -67,7 +58,7 @@ exports.SRT74 = async (prefix) => {
   });
   //  SRT-7.96 -- Draft -> Retirement Initiated
   await switchUser(page, owner, module, headerCategory, category);
-  await draftToRetirementInitiated(page, owner, true);
+  await draftToRetirementInitiated(page, owner);
   await page.waitForTimeout(2000);
   screenshot = 'SRT-7.96_RetirementInitiated.png';
   await page.screenshot({ path: `./tests/screenshots/${screenshot}` });
@@ -76,41 +67,21 @@ exports.SRT74 = async (prefix) => {
     image: screenshot,
   });
   await switchUser(page, approver, module, headerCategory, category);
-  //  SRT-7.110 -- Retirement Initiated -> Approved Retirement
-  await retirementInitiatedToApprovedRetirement(page, approver);
-  await page.waitForTimeout(2000);
-  screenshot = 'SRT-7.110_ApprovedRetirement.png';
+  //  SRT-7.98 -- Retirement Initiated -> Retired
+  await retirementInitiatedToRetired(page, approver);
+  await page.waitForTimeout(4000);
+  screenshot = 'SRT-7.98_Retired.png';
   await page.screenshot({ path: `./tests/screenshots/${screenshot}` });
   results.push({
-    result: `SRT-7.110 -- Retirement Initiated -> Approved Retirement... `,
+    result: `SRT-7.98 -- Retirement Initiated -> Retired... `,
     image: screenshot,
   });
-  //  SRT-7.106 -- Approved Retirement -> Retired
-  await logout(page);
-  await login(page, changeOrderOwner);
-  await openTableView(page, "Quality Management System", "", "category-change-order");
-  await selectTableViewLastChild(page);
-  await draftToReadyForClosure(page, changeOrderOwner);
-  await logout(page);
-  await login(page, changeOrderApprover);
-  await openTableView(page, "Quality Management System", "", "category-change-order");
-  await selectTableViewLastChild(page);
-  await readyForClosureToClosed(page, changeOrderApprover);
-  await openTableView(page, module, headerCategory, category);
-  await selectTableViewLastChild(page);
-  await page.waitForTimeout(1000);
-  screenshot = 'SRT-7.106_Retired.png';
-  await page.screenshot({ path: `./tests/screenshots/${screenshot}` });
-  results.push({
-    result: `SRT-7.106 -- Approved Retirement -> Retired... `,
-    image: screenshot,
-  });
-  
+
   await logout(page);
 
-  createDoc(`SRT-7 4. Retirement with CO ${sort}. ${itemPrefix}`, `SRT-7 Generic Workflow Retirement with CO: ${itemPrefix}`, results);
+  createDoc(`SRT-7 5. Retirement without CO ${sort}. ${itemPrefix}`, `SRT-7 Generic Workflow Retirement without CO: ${itemPrefix}`, results);
 
-  console.log(`SRT-7 4. Retirement with CO ${sort}. ${itemPrefix} test passed`);
+  console.log(`SRT-7 5. Retirement without CO ${sort}. ${itemPrefix} test passed`);
 
   await browser.close();
 
--- server/tests/index.js
+++ server/tests/index.js
@@ -2,10 +2,12 @@ const { SRT71 } = require('../tests/SRT-7-1_normal_w_CO');
 const { SRT72 } = require('../tests/SRT-7-2_normal_wo_CO');
 const { SRT73 } = require('../tests/SRT-7-3_nullifications');
 const { SRT74 } = require('../tests/SRT-7-4_retirement_w_CO');
+const { SRT75 } = require('../tests/SRT-7-5_retirement_wo_CO');
 
 exports.Handler = {
   SRT71,
   SRT72,
   SRT73,
   SRT74,
+  SRT75,
 };
