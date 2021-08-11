0a14c2465101d793e011c3e689c7e460d046cf93
dikuw <michael@dikuw.com>
2021-08-11 10:10:18 -0400

    refactor

--- server/tests/SRT-7-6_retirement_nullifications.js
+++ server/tests/SRT-7-6_retirement_nullifications.js
@@ -18,18 +18,22 @@ const { draftToReadyForClosure, readyForClosureToClosed } = require('../shared/s
 const { draftToRetirementInitiated, retirementInitiatedToRetirementCanceled, retirementInitiatedToRetirementRejected } = require('../shared/shared');
 const { retirementRejectedToDraft, retirementInitiatedToApprovedRetirement } = require('../shared/shared');
 const { createDoc } = require('../shared/createOutput');
+const switchUser = async (page, user, module, headerCategory, category) => {
+  await logout(page);
+  await login(page, user);
+  await openTableView(page, module, headerCategory, category);
+  await selectTableViewLastChild(page);
+};
+
 const { getItemType } = require('../shared/helpers');
 
 exports.SRT76 = async (prefix) => {
-
-  const switchUser = async (page, user, module, headerCategory, category) => {
-    await logout(page);
-    await login(page, user);
-    await openTableView(page, module, headerCategory, category);
-    await selectTableViewLastChild(page);
-  };
-
   let itemType = getItemType(prefix)[0];
+  const { itemPrefix, dataValue, user, owner, approver, module, headerCategory, category, sort } = itemType;
+  
+  let resultsString = "";
+  let results = [];
+  let screenshot = "";
 
   const itemNamePrefix = 'SRT-7 6. Retirement Nullifications';
 
@@ -40,112 +44,133 @@ exports.SRT76 = async (prefix) => {
   });
 
   const page = await browser.newPage();
-  
   await page.goto(tenant);
 
-  const { itemPrefix, dataValue, user, owner, approver, module, headerCategory, category, sort } = itemType;
+  console.log(`❗❗❗ Testing SRT-7 6. Retirement Nullifications ${sort}. ${itemPrefix}...`);
+  console.log(`🕒 Test start time: ${new Date().toLocaleTimeString()}`);
 
-  console.log(`Testing SRT-7 6. Retirement Nullifications ${sort}. ${itemPrefix}...`);
+  try {
+    //  first create the change order for testing
+    await login(page, changeOrderOwner);
+    await createItem(page, "change_order", "SRT-7 4. CO");
+    await logout(page);
 
-  let results = [];
-  let screenshot = "";
+    await login(page, user);
+    //  SRT-7.4 -- Does Not Exist -> Draft
+    await createItem(page, dataValue, itemNamePrefix);
+    await page.waitForTimeout(2000);
+    screenshot = 'SRT-7.4_Draft.png';
+    await page.screenshot({ path: `./tests/screenshots/${screenshot}` });
+    results.push({
+      result: `SRT-7.4 -- Does Not Exist -> Draft... `,
+      resultString: `test passed`,
+      image: screenshot,
+    });
+    //  SRT-7.96 -- Draft -> Retirement Initiated
+    await switchUser(page, owner, module, headerCategory, category);
+    await draftToRetirementInitiated(page, owner, true);
+    await page.waitForTimeout(2000);
+    screenshot = 'SRT-7.96_RetirementInitiated.png';
+    await page.screenshot({ path: `./tests/screenshots/${screenshot}` });
+    results.push({
+      result: `SRT-7.96 -- Draft -> Retirement Initiated... `,
+      resultString: `test passed`,
+      image: screenshot,
+    });
+    //  SRT-7.117 -- Retirement Initiated -> Retirement Canceled
+    await retirementInitiatedToRetirementCanceled(page, owner);
+    await page.waitForTimeout(2000);
+    screenshot = 'SRT-7.117_RetirementCanceled.png';
+    await page.screenshot({ path: `./tests/screenshots/${screenshot}` });
+    results.push({
+      result: `SRT-7.117 -- Retirement Initiated -> Retirement Canceled... `,
+      resultString: `test passed`,
+      image: screenshot,
+    });
+    await draftToRetirementInitiated(page, owner, true);
+    await switchUser(page, approver, module, headerCategory, category);
+    //  SRT-7.105 -- Retirement Initiated -> Retirement Rejected
+    await retirementInitiatedToRetirementRejected(page, approver);
+    await page.waitForTimeout(2000);
+    screenshot = 'SRT-7.105_RetirementRejected.png';
+    await page.screenshot({ path: `./tests/screenshots/${screenshot}` });
+    results.push({
+      result: `SRT-7.105 -- Retirement Initiated -> Retirement Rejected... `,
+      resultString: `test passed`,
+      image: screenshot,
+    });
+    await switchUser(page, owner, module, headerCategory, category);
+    //  SRT-7.7 -- Retirement Rejected -> Draft
+    await retirementRejectedToDraft(page);
+    await page.waitForTimeout(2000);
+    screenshot = 'SRT-7.7_Draft.png';
+    await page.screenshot({ path: `./tests/screenshots/${screenshot}` });
+    results.push({
+      result: `SRT-7.7 -- Retirement Rejected -> Draft... `,
+      resultString: `test passed`,
+      image: screenshot,
+    });
+    await draftToRetirementInitiated(page, owner, true);
+    await switchUser(page, approver, module, headerCategory, category);
+    //  SRT-7.110 -- Retirement Initiated -> Approved Retirement
+    await retirementInitiatedToApprovedRetirement(page, approver);
+    await page.waitForTimeout(2000);
+    screenshot = 'SRT-7.110_ApprovedRetirement.png';
+    await page.screenshot({ path: `./tests/screenshots/${screenshot}` });
+    results.push({
+      result: `SRT-7.110 -- Retirement Initiated -> Approved Retirement... `,
+      resultString: `test passed`,
+      image: screenshot,
+    });
+    //  SRT-7.106 -- Approved Retirement -> Retired
+    await logout(page);
+    await login(page, changeOrderOwner);
+    await openTableView(page, "Quality Management System", "", "category-change-order");
+    await selectTableViewLastChild(page);
+    await draftToReadyForClosure(page, changeOrderOwner);
+    await logout(page);
+    await login(page, changeOrderApprover);
+    await openTableView(page, "Quality Management System", "", "category-change-order");
+    await selectTableViewLastChild(page);
+    await readyForClosureToClosed(page, changeOrderApprover);
+    await openTableView(page, module, headerCategory, category);
+    await selectTableViewLastChild(page);
+    await page.waitForTimeout(1000);
+    screenshot = 'SRT-7.106_Retired.png';
+    await page.screenshot({ path: `./tests/screenshots/${screenshot}` });
+    results.push({
+      result: `SRT-7.106 -- Approved Retirement -> Retired... `,
+      resultString: `test passed`,
+      image: screenshot,
+    });
+    await logout(page);
 
-  //  first create the change order for testing
-  await login(page, changeOrderOwner);
-  await createItem(page, "change_order", "SRT-7 4. CO");
-  await logout(page);
+    createDoc(`SRT-7 6. Retirement Nullifications ${sort}. ${itemPrefix}`, `SRT-7 Generic Workflow Retirement Nullifications: ${itemPrefix}`, results);
+    resultsString = `🙌 SRT-7 6. Retirement Nullification ${sort}. ${itemPrefix} test passed`;
 
-  await login(page, user);
-  //  SRT-7.4 -- Does Not Exist -> Draft
-  await createItem(page, dataValue, itemNamePrefix);
-  await page.waitForTimeout(2000);
-  screenshot = 'SRT-7.4_Draft.png';
-  await page.screenshot({ path: `./tests/screenshots/${screenshot}` });
-  results.push({
-    result: `SRT-7.4 -- Does Not Exist -> Draft... `,
-    image: screenshot,
-  });
-  //  SRT-7.96 -- Draft -> Retirement Initiated
-  await switchUser(page, owner, module, headerCategory, category);
-  await draftToRetirementInitiated(page, owner, true);
-  await page.waitForTimeout(2000);
-  screenshot = 'SRT-7.96_RetirementInitiated.png';
-  await page.screenshot({ path: `./tests/screenshots/${screenshot}` });
-  results.push({
-    result: `SRT-7.96 -- Draft -> Retirement Initiated... `,
-    image: screenshot,
-  });
-  //  SRT-7.117 -- Retirement Initiated -> Retirement Canceled
-  await retirementInitiatedToRetirementCanceled(page, owner);
-  await page.waitForTimeout(2000);
-  screenshot = 'SRT-7.117_RetirementCanceled.png';
-  await page.screenshot({ path: `./tests/screenshots/${screenshot}` });
-  results.push({
-    result: `SRT-7.117 -- Retirement Initiated -> Retirement Canceled... `,
-    image: screenshot,
-  });
-  await draftToRetirementInitiated(page, owner, true);
-  await switchUser(page, approver, module, headerCategory, category);
-  //  SRT-7.105 -- Retirement Initiated -> Retirement Rejected
-  await retirementInitiatedToRetirementRejected(page, approver);
-  await page.waitForTimeout(2000);
-  screenshot = 'SRT-7.105_RetirementRejected.png';
-  await page.screenshot({ path: `./tests/screenshots/${screenshot}` });
-  results.push({
-    result: `SRT-7.105 -- Retirement Initiated -> Retirement Rejected... `,
-    image: screenshot,
-  });
-  await switchUser(page, owner, module, headerCategory, category);
-  //  SRT-7.7 -- Retirement Rejected -> Draft
-  await retirementRejectedToDraft(page);
-  await page.waitForTimeout(2000);
-  screenshot = 'SRT-7.7_Draft.png';
-  await page.screenshot({ path: `./tests/screenshots/${screenshot}` });
-  results.push({
-    result: `SRT-7.7 -- Retirement Rejected -> Draft... `,
-    image: screenshot,
-  });
-  await draftToRetirementInitiated(page, owner, true);
-  await switchUser(page, approver, module, headerCategory, category);
-  //  SRT-7.110 -- Retirement Initiated -> Approved Retirement
-  await retirementInitiatedToApprovedRetirement(page, approver);
-  await page.waitForTimeout(2000);
-  screenshot = 'SRT-7.110_ApprovedRetirement.png';
-  await page.screenshot({ path: `./tests/screenshots/${screenshot}` });
-  results.push({
-    result: `SRT-7.110 -- Retirement Initiated -> Approved Retirement... `,
-    image: screenshot,
-  });
-  //  SRT-7.106 -- Approved Retirement -> Retired
-  await logout(page);
-  await login(page, changeOrderOwner);
-  await openTableView(page, "Quality Management System", "", "category-change-order");
-  await selectTableViewLastChild(page);
-  await draftToReadyForClosure(page, changeOrderOwner);
-  await logout(page);
-  await login(page, changeOrderApprover);
-  await openTableView(page, "Quality Management System", "", "category-change-order");
-  await selectTableViewLastChild(page);
-  await readyForClosureToClosed(page, changeOrderApprover);
-  await openTableView(page, module, headerCategory, category);
-  await selectTableViewLastChild(page);
-  await page.waitForTimeout(1000);
-  screenshot = 'SRT-7.106_Retired.png';
-  await page.screenshot({ path: `./tests/screenshots/${screenshot}` });
-  results.push({
-    result: `SRT-7.106 -- Approved Retirement -> Retired... `,
-    image: screenshot,
-  });
-  
-  await logout(page);
+  } catch (err) {
+    
+    screenshot = 'error.png';
+    await page.screenshot({ path: `./tests/screenshots/${screenshot}` });
+    results.push({
+      result: `ERROR... `,
+      resultString: `test error`,
+      image: screenshot,
+    });
 
-  createDoc(`SRT-7 6. Retirement Nullifications ${sort}. ${itemPrefix}`, `SRT-7 Generic Workflow Retirement Nullifications: ${itemPrefix}`, results);
+    createDoc(`ERROR SRT-7 6. Retirement Nullifications ${sort}. ${itemPrefix}`, `ERROR: SRT-7 Generic Workflow Retirement Nullifications: ${itemPrefix}`, results);
+    resultsString = `❌ ERROR: SRT-7 6. Retirement Nullifications ${sort}. ${itemPrefix}: ${err}`;
 
-  console.log(`SRT-7 6. Retirement Nullifications ${sort}. ${itemPrefix} test passed`);
+  } finally {
+    
+    await browser.close();
 
-  await browser.close();
+    console.log(`🕕 Test end time: ${new Date().toLocaleTimeString()}`);
+    console.log(resultsString);
 
-  return `SRT-7 6. Retirement Nullifications ${sort}. ${itemPrefix} test passed`;
+    return resultsString;
+
+  }
 
 }
 
