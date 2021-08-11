b876e8248e67839e78c46563bb34998bcbbb7a4b
dikuw <michael@dikuw.com>
2021-08-11 09:58:40 -0400

    refactored

--- server/tests/SRT-7-5_retirement_wo_CO.js
+++ server/tests/SRT-7-5_retirement_wo_CO.js
@@ -14,18 +14,23 @@ const { selectTableViewLastChild } = require('../shared/shared');
 const { draftToRetirementInitiated } = require('../shared/shared');
 const { retirementInitiatedToRetired } = require('../shared/shared');
 const { createDoc } = require('../shared/createOutput');
+
+const switchUser = async (page, user, module, headerCategory, category) => {
+  await logout(page);
+  await login(page, user);
+  await openTableView(page, module, headerCategory, category);
+  await selectTableViewLastChild(page);
+};
+
 const { getItemType } = require('../shared/helpers');
 
 exports.SRT75 = async (prefix) => {
-
-  const switchUser = async (page, user, module, headerCategory, category) => {
-    await logout(page);
-    await login(page, user);
-    await openTableView(page, module, headerCategory, category);
-    await selectTableViewLastChild(page);
-  };
-
   let itemType = getItemType(prefix)[0];
+  const { itemPrefix, dataValue, user, owner, approver, module, headerCategory, category, sort } = itemType;
+  
+  let resultsString = "";
+  let results = [];
+  let screenshot = "";
 
   const itemNamePrefix = 'SRT-7 5. Retirement without CO';
 
@@ -36,56 +41,74 @@ exports.SRT75 = async (prefix) => {
   });
 
   const page = await browser.newPage();
-  
   await page.goto(tenant);
 
-  const { itemPrefix, dataValue, user, owner, approver, module, headerCategory, category, sort } = itemType;
+  console.log(`❗❗❗ Testing SRT-7 5. Retirement without CO ${sort}. ${itemPrefix}...`);
+  console.log(`🕒 Test start time: ${new Date().toLocaleTimeString()}`);
 
-  console.log(`Testing SRT-7 5. Retirement without CO ${sort}. ${itemPrefix}...`);
+  try {
 
-  let results = [];
-  let screenshot = "";
+    await login(page, user);
+    //  SRT-7.4 -- Does Not Exist -> Draft
+    await createItem(page, dataValue, itemNamePrefix);
+    await page.waitForTimeout(2000);
+    screenshot = 'SRT-7.4_Draft.png';
+    await page.screenshot({ path: `./tests/screenshots/${screenshot}` });
+    results.push({
+      result: `SRT-7.4 -- Does Not Exist -> Draft... `,
+      resultString: `test passed`,
+      image: screenshot,
+    });
+    //  SRT-7.96 -- Draft -> Retirement Initiated
+    await switchUser(page, owner, module, headerCategory, category);
+    await draftToRetirementInitiated(page, owner);
+    await page.waitForTimeout(2000);
+    screenshot = 'SRT-7.96_RetirementInitiated.png';
+    await page.screenshot({ path: `./tests/screenshots/${screenshot}` });
+    results.push({
+      result: `SRT-7.96 -- Draft -> Retirement Initiated... `,
+      resultString: `test passed`,
+      image: screenshot,
+    });
+    await switchUser(page, approver, module, headerCategory, category);
+    //  SRT-7.98 -- Retirement Initiated -> Retired
+    await retirementInitiatedToRetired(page, approver);
+    await page.waitForTimeout(4000);
+    screenshot = 'SRT-7.98_Retired.png';
+    await page.screenshot({ path: `./tests/screenshots/${screenshot}` });
+    results.push({
+      result: `SRT-7.98 -- Retirement Initiated -> Retired... `,
+      resultString: `test passed`,
+      image: screenshot,
+    });
+    await logout(page);
 
-  await login(page, user);
-  //  SRT-7.4 -- Does Not Exist -> Draft
-  await createItem(page, dataValue, itemNamePrefix);
-  await page.waitForTimeout(2000);
-  screenshot = 'SRT-7.4_Draft.png';
-  await page.screenshot({ path: `./tests/screenshots/${screenshot}` });
-  results.push({
-    result: `SRT-7.4 -- Does Not Exist -> Draft... `,
-    image: screenshot,
-  });
-  //  SRT-7.96 -- Draft -> Retirement Initiated
-  await switchUser(page, owner, module, headerCategory, category);
-  await draftToRetirementInitiated(page, owner);
-  await page.waitForTimeout(2000);
-  screenshot = 'SRT-7.96_RetirementInitiated.png';
-  await page.screenshot({ path: `./tests/screenshots/${screenshot}` });
-  results.push({
-    result: `SRT-7.96 -- Draft -> Retirement Initiated... `,
-    image: screenshot,
-  });
-  await switchUser(page, approver, module, headerCategory, category);
-  //  SRT-7.98 -- Retirement Initiated -> Retired
-  await retirementInitiatedToRetired(page, approver);
-  await page.waitForTimeout(4000);
-  screenshot = 'SRT-7.98_Retired.png';
-  await page.screenshot({ path: `./tests/screenshots/${screenshot}` });
-  results.push({
-    result: `SRT-7.98 -- Retirement Initiated -> Retired... `,
-    image: screenshot,
-  });
+    createDoc(`SRT-7 5. Retirement without CO ${sort}. ${itemPrefix}`, `SRT-7 Generic Workflow Retirement without CO: ${itemPrefix}`, results);
+    resultsString = `🙌 SRT-7 5. Retirement without CO ${sort}. ${itemPrefix} test passed`;
 
-  await logout(page);
+  } catch (err) {
+    
+    screenshot = 'error.png';
+    await page.screenshot({ path: `./tests/screenshots/${screenshot}` });
+    results.push({
+      result: `ERROR... `,
+      resultString: `test error`,
+      image: screenshot,
+    });
 
-  createDoc(`SRT-7 5. Retirement without CO ${sort}. ${itemPrefix}`, `SRT-7 Generic Workflow Retirement without CO: ${itemPrefix}`, results);
+    createDoc(`ERROR SRT-7 5. Retirement without CO ${sort}. ${itemPrefix}`, `ERROR: SRT-7 Generic Workflow Retirement without CO: ${itemPrefix}`, results);
+    resultsString = `❌ ERROR: SRT-7 5. Retirement without CO ${sort}. ${itemPrefix}: ${err}`;
 
-  console.log(`SRT-7 5. Retirement without CO ${sort}. ${itemPrefix} test passed`);
+  } finally {
+    
+    await browser.close();
 
-  await browser.close();
+    console.log(`🕕 Test end time: ${new Date().toLocaleTimeString()}`);
+    console.log(resultsString);
 
-  return `SRT-7 5. Retirement without CO ${sort}. ${itemPrefix} test passed`;
+    return resultsString;
+
+  }
 
 };
 
